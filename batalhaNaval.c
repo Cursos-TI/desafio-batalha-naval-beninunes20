609a80a119ab7455706b4efc691df35308bea963
beninunes20 <beni.nunes20@gmail.com>
2026-04-21 20:08:05 +0000

    Conclusão do desafio: Nível novato Criação do tabuleiro, alocação de dois navios um na vertical e um na horizontal e exibição do tabuleiro final.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define RED   "\x1b[31m"
+#define RESET "\x1b[0m"
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
@@ -9,7 +11,63 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    /*Declaração de variáveis*/
+    char colunas_auxiliar[10] = {'a','b','c','d','e','f','g','h','i','j'}; // Letras para identificação das colunas no tabuleiro
+    int linhas_auxiliar[10] = {0,1,2,3,4,5,6,7,8,9}; //Números para identificaçaõ das linhas no tabuleiro
+    int tabuleiro[10][10]; //Definição do tabuleiro
+    int valor_inicial = 0; // Valor inicial de todo o tabuleiro, 0 foi considerado o padrão para água 
+    int colunas = 0; // Variável de controle para o loop while 
+    int navios[3] =  {3,3,3}; 
 
+    /*Atribuição do valor inicial para toda a matriz*/
+    for (int i = 0; i < 10; i++) // Loop para linhas
+    {
+        for (int j = 0; j < 10; j++) // Loop para colunas 
+        {
+            tabuleiro[i][j] = valor_inicial; // Atribuição do valor inicial (0) para toda a matriz
+        }
+    }
+
+    /*Alocando os navios na matriz*/
+    //Navio horizontal
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro [6][2+i] = navios[i];
+    }
+
+    //Navio na vertical
+
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro [2+i][5] = navios[i];
+    }
+    
+
+    /*Exibição do tabuleiro */
+    printf(RED "+----------------------------------------------+\n" RESET); // Bordas do tabuleiro
+    printf( RED "|      " RESET);// Bordas do tabuleiro
+
+     while (colunas < 10)
+     {
+        printf(RED "%-3c " RESET, colunas_auxiliar[colunas]); // Exibição das letras de identificação das colunas 
+        colunas++; // incremento da variável de controle  
+     }
+     printf( RED "| " RESET);// Bordas do tabuleiro
+     printf("\n");
+     
+     /*Loop para exibição do tabuleiro e atribuição do valor inicial para toda a matriz*/
+    for (int i = 0; i < 10; i++) // Loop para linhas
+    {
+        printf( RED "| " RESET);// Bordas do tabuleiro
+        printf(RED "%d    "RESET, linhas_auxiliar[i]);// Bordas do tabuleiro
+        for (int j = 0; j < 10; j++) // Loop para colunas 
+        {
+            printf("%-4d", tabuleiro[i][j]); // Exibição do tabuleiro(Matriz) com espaçãmento de -4
+        }
+        printf(RED "| \n" RESET);// Bordas do tabuleiro
+    }
+
+    printf(RED "+----------------------------------------------+\n" RESET);// Bordas do tabuleiro
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
